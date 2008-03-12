17d3497f39d20309f6f9bfd2241bfd65021fb764
jeremy.prz <jeremy.prz@b7b21b0b-af19-0410-a0d9-6f5e44ee2930>
2008-03-12 19:39:11 +0000

    Upgraded to GWT 1.4.61 Removed ant-gwt.jar (seems to be broken either in Leopard or with GWT 1.4.61, not sure)

--- gwt-spring-hibernate-ant-reference/src/com/mycompany/proto/console/hello/client/HelloServiceAsync.java
+++ gwt-spring-hibernate-ant-reference/src/com/mycompany/proto/console/hello/client/HelloServiceAsync.java
@@ -1,8 +1,5 @@
 package com.mycompany.proto.console.hello.client;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface HelloServiceAsync {
