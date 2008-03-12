55d6f02cd374b09065927b5ef3244279b5cec7c8
jeremy.prz <jeremy.prz@b7b21b0b-af19-0410-a0d9-6f5e44ee2930>
2008-03-12 19:39:11 +0000

    Upgraded to GWT 1.4.61 Removed ant-gwt.jar (seems to be broken either in Leopard or with GWT 1.4.61, not sure)

--- trunk/gwt-spring-hibernate-ant-reference/src/com/mycompany/proto/console/hello/client/CustomerServiceAsync.java
+++ trunk/gwt-spring-hibernate-ant-reference/src/com/mycompany/proto/console/hello/client/CustomerServiceAsync.java
@@ -1,8 +1,13 @@
 package com.mycompany.proto.console.hello.client;
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface CustomerServiceAsync {
+	
+	/**
+	 */
 	void getCustomers(AsyncCallback callback);
 	void createCustomer(String firstName, String lastName, AsyncCallback callback);
 	void deleteCustomer(CustomerAdaptor customer, AsyncCallback callback);
